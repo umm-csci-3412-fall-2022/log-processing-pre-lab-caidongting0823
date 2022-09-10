a7abe987342431d87f02ca6fa3e9992ee07c4668
Dongting Cai <cai00151@morris.umn.edu>
2022-09-10 18:21:13 -0500

    Update regex.sh, with ShellCheck improvements
    < Issue #4 >
    Update regex.sh, which contains improvements that ShellCheck noticed.

--- regex/regex.sh
+++ regex/regex.sh
@@ -5,7 +5,7 @@ r0=$(sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt)
 echo "$r0" > r0_output.txt
 
 # Filter for r1
-cat r1_input.txt | sed -n -E 's/\*.* ([A-Z][A-Za-z]+)\. .* ([a-z]+)\./1. \1\n2. \2\n/p' > r1_output.txt
+sed -n -E 's/\*.* ([A-Z][A-Za-z]+)\. .* ([a-z]+)\./1. \1\n2. \2\n/p' < r1_input.txt > r1_output.txt
 
 # Remark: Not work yet - only for future reference
 #cat r1_input.txt | awk 'match($0, /\*.* ([A-Z][A-Za-z]*)\. .* ([a-z]+)\./, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' > r1_output.txt
@@ -20,4 +20,4 @@ cat r1_input.txt | sed -n -E 's/\*.* ([A-Z][A-Za-z]+)\. .* ([a-z]+)\./1. \1\n2. 
 #cat r2_input.txt | sed -E 's/\*.* ((?:[A-Za-z]+(?:\.)+)+) ([a-z]+ [a-z]+)/1. \1\n2. \2\n/' > r2_output.txt
 #cat r2_input.txt | awk -E 'match($0, /\*.*\s((?:[A-Za-z]+(?:\.)+)+)\s([a-z]+\s[a-z]+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' > r2_output.txt
 
-cat r2_input.txt | sed -E 's/\* sandwich with ([A-Za-z.]+) ([a-z ]+)/1. \1\n2. \2\n/' > r2_output.txt
+sed -E 's/\* sandwich with ([A-Za-z.]+) ([a-z ]+)/1. \1\n2. \2\n/' < r2_input.txt > r2_output.txt
